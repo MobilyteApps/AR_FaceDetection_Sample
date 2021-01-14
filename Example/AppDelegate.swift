96b5a304b21aa9904803104ced5f52c57d9ba96c
MobilyteApps <39619790+MobilyteApps@users.noreply.github.com>
2021-01-15 02:48:43 +0530

    Face detection sample

---
+++ Example/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Infostride
+//  Copyright © Infostride. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+}
